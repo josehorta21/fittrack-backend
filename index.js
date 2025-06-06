8d393b5058dc427e5e21424b92197d603da1c2ca
Jose Horta <joseantoniohorta@Joses-MacBook-Pro.local>
2025-06-06 03:09:37 -0700

    feat: added auth routes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+
 
 // Simple route
 app.get("/", (req, res) => {

f99964037eeb4a2a9292c73a7805934253bfc770
Jose Horta <joseantoniohorta@Joses-MacBook-Pro.local>
2025-06-11 21:28:40 -0700

    Cleaned up auth.js and removed duplicate version

--- src/routes/workoutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware');
-const { getWorkouts } = require('../controllers/workoutController');
-
-router.get('/', authMiddleware, getWorkouts);
-
-module.exports = router;
